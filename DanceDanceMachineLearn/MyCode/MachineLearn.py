3a247f9a61cd7a24daffa6ca595697303fcf976d
Kelvin <kelvinnpc@gmail.com>
2017-09-19 15:23:49 +0800

    resolved auto download bug

--- DanceDanceMachineLearn/MyCode/MachineLearn.py
+++ DanceDanceMachineLearn/MyCode/MachineLearn.py
@@ -12,6 +12,8 @@ import tensorflow as tf
 # Data sets
 IRIS_TRAINING = "iris_training.csv"
 IRIS_TRAINING_URL = "http://download.tensorflow.org/data/iris_training.csv"
+RUNWALK_TRAINING = "runWalk.csv"
+RUNWALK_TRAINING_URL = "https://www.kaggle.com/vmalyi/run-or-walk/downloads/dataset.csv"
 
 IRIS_TEST = "iris_test.csv"
 IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
@@ -19,18 +21,19 @@ IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
 def main():
   # If the training and test sets aren't stored locally, download them.
   if not os.path.exists(IRIS_TRAINING):
-    raw = urllib.request.urlopen(IRIS_TRAINING_URL).read()
+    raw = urllib.request.urlopen(IRIS_TRAINING_URL).read().decode("utf-8")
     with open(IRIS_TRAINING, "w") as f:
       f.write(raw)
 
   if not os.path.exists(IRIS_TEST):
-    raw = urllib.request.urlopen(IRIS_TEST_URL).read()
+    raw = urllib.request.urlopen(IRIS_TEST_URL).read().decode("utf-8")
     with open(IRIS_TEST, "w") as f:
       f.write(raw)
 
   # Load datasets.
   training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
       filename=IRIS_TRAINING,
+#       filename=RUNWALK_TRAINING,
       target_dtype=np.int,
       features_dtype=np.float32)
   test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
