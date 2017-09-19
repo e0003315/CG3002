d455bff86325e38bf3389cb9cb46d63cc4e3ec17
CheeYeo[wl] <tehcy2@msn.com>
2017-09-19 13:18:26 +0800

    check if files exist

--- DanceDanceMachineLearn/MyCode/MachineLearn.py
+++ DanceDanceMachineLearn/MyCode/MachineLearn.py
@@ -10,23 +10,31 @@ import numpy as np
 import tensorflow as tf
 
 # Data sets
-IRIS_TRAINING = "iris_training.csv"
+RUNWALK_TRAINING = "runwalk_training.csv" #IRIS_TRAINING = "iris_training.csv"
 IRIS_TRAINING_URL = "http://download.tensorflow.org/data/iris_training.csv"
 
-IRIS_TEST = "iris_test.csv"
+RUNWALK_TEST = "runwalk_test.csv" #IRIS_TEST = "iris_test.csv"
 IRIS_TEST_URL = "http://download.tensorflow.org/data/iris_test.csv"
 
 def main():
   # If the training and test sets aren't stored locally, download them.
-  if not os.path.exists(IRIS_TRAINING):
-    raw = urllib.request.urlopen(IRIS_TRAINING_URL).read()
-    with open(IRIS_TRAINING, "w") as f:
-      f.write(raw)
+  # if not os.path.exists(IRIS_TRAINING):
+  #   raw = urllib.request.urlopen(IRIS_TRAINING_URL).read()
+  #   with open(IRIS_TRAINING, "w") as f:
+  #     f.write(raw)
 
-  if not os.path.exists(IRIS_TEST):
-    raw = urllib.request.urlopen(IRIS_TEST_URL).read()
-    with open(IRIS_TEST, "w") as f:
-      f.write(raw)
+  # if not os.path.exists(IRIS_TEST):
+  #   raw = urllib.request.urlopen(IRIS_TEST_URL).read()
+  #   with open(IRIS_TEST, "w") as f:
+  #     f.write(raw)
+	
+  # If the training and test sets aren't stored locally, report error.
+  if not os.path.exists(RUNWALK_TRAINING):
+  	print("Training file does not exist. \n")
+
+  if not os.path.exists(RUNWALK_TEST):
+  	print("Test file does not exist. \n")
+
 
   # Load datasets.
   training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
