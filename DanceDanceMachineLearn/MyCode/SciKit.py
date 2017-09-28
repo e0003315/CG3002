5b690c49bf5a143be14ef79dba810d630177574d
CheeYeo[wl] <tehcy2@msn.com>
2017-09-28 14:30:23 +0800

    removed cart and svc

--- DanceDanceMachineLearn/MyCode/SciKit.py
+++ DanceDanceMachineLearn/MyCode/SciKit.py
@@ -74,13 +74,14 @@ models = []
 # models.append(('LR', LogisticRegression()))
 # models.append(('LDA', LinearDiscriminantAnalysis()))
 models.append(('KNN', KNeighborsClassifier(n_neighbors = 8)))
-models.append(('CART', DecisionTreeClassifier()))
+# models.append(('CART', DecisionTreeClassifier()))
 # models.append(('NB', GaussianNB()))
 # models.append(('SVM', SVC()))
+
 # evaluate each model in turn
 results = []
+
 names = []
-for name, model in models:
     kfold = model_selection.KFold(n_splits=10, random_state=seed)
     cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
     results.append(cv_results)
@@ -92,17 +93,9 @@ for name, model in models:
 knn = KNeighborsClassifier()
 knn.fit(X_train, Y_train)
 predictions = knn.predict(X_validation)
-# print(predictions)
 print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
-  
-# svm = SVC()
-# svm.fit(X_train, Y_train)
-# predictSVM = svm.predict(X_validation)
-# print(accuracy_score(Y_validation, predictSVM))
-# print(confusion_matrix(Y_validation, predictSVM))
-# print(classification_report(Y_validation, predictSVM))
 
 a = input()
 while (a != "0"):
