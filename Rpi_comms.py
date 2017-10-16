bd2573bc0d30b6166ea1aa20f62a577f724a73f2
zavier <zacohjayjay@gmail.com>
2017-10-16 20:40:43 +0800

    Integrated Wireless and Serial communication to work in 1 file

--- Rpi_comms.py
+++ Rpi_comms.py
@@ -1,22 +1,36 @@
-#import serial_communication
+import serial_communication_checksum
 import auth_client
 import sys
+import socket
 
 class Rpi_comms:
 
     def __init__(self, ip_addr, port_num):
-
+        ready = False
         #init serial comms
-        #self.Scomms = serial_communication.serial_communications()
+        self.Scomms = serial_communication_checksum.serial_communication()
         #init wireless comms
         self.Wcomms = auth_client.client(ip_addr, port_num)
-       
+        print('init Wcomms')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (ip_addr, port_num)
+        print(server_address)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(server_address)
+
+        while ready == False:
+            print('no handshake')
+            ready = self.Scomms.handshake()
+            
         #receivedData = self.Scomms.receiveData();
-        while True:
+        while ready:
             try:
-                receivedData = 'wavehands|7|7|7|7|'
+                receivedData = self.Scomms.receiveData()
+                receivedData1 = input('enter plaintext')
+                #receivedData1 = 'wavehands|7|7|7|7|'
                 print(receivedData)
-                self.Wcomms.sendData(receivedData)
+                msg = self.Wcomms.packData(receivedData1)
+                sock.sendall(msg)
             except Exception as e:
                 print(e)
                 
