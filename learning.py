fc6317a437c7e11730dc5d681a79e181bb2d6530
Kelvin <kelvinnpc@gmail.com>
2017-10-24 17:39:28 +0800

    ML training working

--- learning.py
+++ learning.py
@@ -21,6 +21,11 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from cProfile import label
 
+pandas.set_option('display.height', 1000)
+pandas.set_option('display.max_rows', 500)
+pandas.set_option('display.max_columns', 500)
+pandas.set_option('display.width', 1000)
+pandas.set_option('display.expand_frame_repr', False)
 class learning:
     def _init_(self):
         super(learning, self)._init_()
@@ -44,14 +49,14 @@ class learning:
     def machineTrain(self):
         # Load dataset
         # url = "C:/Users/CheeYeo/Desktop/CG3002/Code/Test case/TenMoves.csv" #CY's computer file path
-        url = "C:/Users/User/Documents/SEM5/CG3002/Project3002/Week11 Readings/CompiledMoves11.csv"
-        valiUrl = "C:/Users/User/Documents/SEM5/CG3002/Project3002/Week11 Readings/CompiledMoves11.csv"
+        url = "C:/Users/User/Documents/SEM5/CG3002/Project3002/Week11 Readings/6pplData.csv"
+        valiUrl = "C:/Users/User/Documents/SEM5/CG3002/Project3002/Week11 Readings/ValidationDataSetWeek11.csv"
 #         url = "C:/Users/User/Documents/SEM5/CG3002/Project3002/Test case/CompiledMoves.csv"  # Kelvin's computer file path
         # url = "/home/pi/Desktop/CompiledMoves.csv"
         # names = ['accel_x', 'accel_y', 'accel_z', 'gyro_x', 'gyro_y', 'gyro_z', 'activity']
         # dataset = pandas.read_csv(url, names=names)
         dataset = pandas.read_csv(url)
-        validation = pandas.read_csv(valiUrl)
+#         validation = pandas.read_csv(valiUrl)
         global window_size
         window_size = 60
         shift_size = 30
@@ -60,14 +65,15 @@ class learning:
         array = dataset.values
         X = array[:, 0:12]
         Y = array[:, 12]
-        array = validation.values
-        VALI_X = array[:, 0:12]
-        VALI_Y = array[:, 12]
+#         array = validation.values
+#         VALI_X = array[:, 0:12]
+#         VALI_Y = array[:, 12]
         # label encode
         global le 
         le = preprocessing.LabelEncoder()
         le.fit(['NoMove', 'WaveHand', 'BusDrive', 'FrontBack', 'SideStep', 'Jumping', 'jumpingJack', 'turnClap', 'squatTurnClap', 'window', 'window360'])
         Y_encoded = le.transform(Y)
+#         VALI_Y = le.transform(VALI_Y)
         
         # print(datetime.datetime.now().time())
         
@@ -76,6 +82,7 @@ class learning:
         K = (N // shift_size) - 1
         segments_X = numpy.empty((K, window_size, dim_X))
         segments_Y = numpy.empty((K, window_size))
+        
         for i in range(K):
             segment_X = X[i * shift_size : (i * shift_size) + window_size , :]
             segment_X = preprocessing.normalize(segment_X)
@@ -83,39 +90,41 @@ class learning:
             segments_X[i] = segment_X
             segments_Y[i] = segment_Y
         
-        N = validation.shape[0]
-        dim_X = VALI_X.shape[1]
-        K = (N // shift_size) - 1
-        segments_X = numpy.empty((K, window_size, dim_X))
-        segments_Y = numpy.empty((K, window_size))
-        for i in range(K):
-            segment_X = VALI_X[i * shift_size : (i * shift_size) + window_size , :]
-            segment_X = preprocessing.normalize(segment_X)
-            segment_Y = VALI_Y[i * shift_size : (i * shift_size) + window_size]
-            VALI_segments_X[i] = segment_X
-            VALI_segments_Y[i] = segment_Y
+#         N = validation.shape[0]
+#         dim_X = VALI_X.shape[1]
+#         K = (N // shift_size) - 1
+#         VALI_segments_X = numpy.empty((K, window_size, dim_X))
+#         VALI_segments_Y = numpy.empty((K, window_size))
+#         for i in range(K):
+#             segment_X = VALI_X[i * shift_size : (i * shift_size) + window_size , :]
+#             segment_X = preprocessing.normalize(segment_X)
+#             segment_Y = VALI_Y[i * shift_size : (i * shift_size) + window_size]
+#             VALI_segments_X[i] = segment_X
+#             VALI_segments_Y[i] = segment_Y
         
         features = numpy.empty((K, 36))
         outputs = numpy.empty((K))
-        VALI_features = numpy.empty((K, 36))
-        VALI_outputs = numpy.empty((K))
+#         VALI_features = numpy.empty((K, 36))
+#         VALI_outputs = numpy.empty((K))
         
         for i in range(K):
             for j in range(0, features.shape[1] - 1, 3):
                 features[i, j] = segments_X[i, : , j // 3].mean()
                 features[i, j + 1] = segments_X[i, : , j // 3].std()
                 features[i, j + 2] = self.mad(data = segments_X[i, : , j // 3])
-                VALI_features[i, j] = VALI_segments_X[i, : , j // 3].mean()
-                VALI_features[i, j + 1] = VALI_segments_X[i, : , j // 3].std()
-                VALI_features[i, j + 2] = self.mad(data = VALI_segments_X[i, : , j // 3])
+#                 VALI_features[i, j] = VALI_segments_X[i, : , j // 3].mean()
+#                 VALI_features[i, j + 1] = VALI_segments_X[i, : , j // 3].std()
+#                 VALI_features[i, j + 2] = self.mad(data = VALI_segments_X[i, : , j // 3])
             outputs[i] = stats.mode(segments_Y[i])[0]
-            VALI_outputs[i] = stats.mode(VALI_segments_Y[i])[0]
-        
+#             VALI_outputs[i] = stats.mode(VALI_segments_Y[i])[0]
+#         with pandas.option_context('display.max_rows', -1, 'display.max_columns', 5):
+#             print(features[197, :], file=open('feature.txt', 'a'))
+#         print(outputs, file=open('output.txt', 'w'))
             
         # print(datetime.datetime.now().time())
         
-        validation_size = 0
-        seed = 7
+        validation_size = 0.2
+        seed = 5
         X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(features, outputs, test_size=validation_size, random_state=seed)
         
         # Test options and evaluation metric
@@ -145,16 +154,16 @@ class learning:
         # Make predictions on validation dataset
         knn = KNeighborsClassifier(n_neighbors=3)
         knn.fit(X_train, Y_train)
-#         predictions = knn.predict(X_validation)
-        predictions = knn.predict(VALI_features)
-#         print("Accuracy Score: ", accuracy_score(Y_validation, predictions), file=open('summary.txt', 'a'))
-        print("Accuracy Score: ", accuracy_score(VALI_outputs, predictions), file=open('summary.txt', 'a'))
+        predictions = knn.predict(X_validation)
+#         predictions = knn.predict(VALI_features)
+        print("Accuracy Score: ", accuracy_score(Y_validation, predictions), file=open('summary.txt', 'a'))
+#         print("Accuracy Score: ", accuracy_score(VALI_outputs, predictions), file=open('summary.txt', 'a'))
         print("")
-#         print("Confusion Matrix: \n", confusion_matrix(Y_validation, predictions, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), file=open('summary.txt', 'a'))
-        print("Confusion Matrix: \n", confusion_matrix(VALI_outputs, predictions, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), file=open('summary.txt', 'a'))
+        print("Confusion Matrix: \n", confusion_matrix(Y_validation, predictions, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), file=open('summary.txt', 'a'))
+#         print("Confusion Matrix: \n", confusion_matrix(VALI_outputs, predictions, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), file=open('summary.txt', 'a'))
         print("")
-#         print("Classification Report: \n", classification_report(Y_validation, predictions), file=open('summary.txt', 'a'))
-        print("Classification Report: \n", classification_report(VALI_outputs, predictions), file=open('summary.txt', 'a'))
+        print("Classification Report: \n", classification_report(Y_validation, predictions), file=open('summary.txt', 'a'))
+#         print("Classification Report: \n", classification_report(VALI_outputs, predictions), file=open('summary.txt', 'a'))
         
 #         print("TEST\n")
 #         print(self.processData(segment_X, knn))
@@ -162,22 +171,24 @@ class learning:
         
         return knn
 #         a = input()
-#         # rawData = numpy.empty(80, 12)
-#         # featureData = numpy.empty(24)
-#         while (a != "0"):
-#         #     counter = 0
-#         #     while (counter < 80):
+#         rawData = numpy.empty(80, 12)
+#         featureData = numpy.empty(24)
+#         print("preWhile")
+#         while (True):
+# #             counter = 0
+# #             while (counter < 80):
 #             my_list = a.split()
 #             matrix = [my_list]
-#         #         rawData[counter] = matrix
-#         #         counter = counter + 1
+# #         #         rawData[counter] = matrix
+# #         #         counter = counter + 1
 #             predictions = knn.predict(matrix)
-#             
-#         #     for j in range(0, featureData.shape[1] - 1, 2):
-#         #         featureData[j] = rawData[: , j//2].mean()
-#         #         featureData[j+1] = rawData[: , j//2].std()
-#         
-#         #     predictions = knn.predict(featureData)
+#             predictions = le.inverse_transform([int(predictions[0])])
+# #             
+# #         #     for j in range(0, featureData.shape[1] - 1, 2):
+# #         #         featureData[j] = rawData[: , j//2].mean()
+# #         #         featureData[j+1] = rawData[: , j//2].std()
+# #         
+# #         #     predictions = knn.predict(featureData)
 #             print(predictions)
 #             a = input()
 
