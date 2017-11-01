55ecb718a88ec200c531d612ab6e2ebea3375835
CheeYeo[wl] <tehcy2@msn.com>
2017-11-01 10:19:56 +0800

    Fixed If All statement

--- Rpi_comms.py
+++ Rpi_comms.py
@@ -45,7 +45,7 @@ class Rpi_comms:
                     moveConcluded[consecutiveCount] = move
                     consecutiveCount = (consecutiveCount + 1) % 5
                     # print(move)
-                    if ((x == moveConcluded[0] for x in moveConluded) && x != ["NoMove"])	
+                    if (all((x != ["NoMove"] and x == moveConcluded[0] for x in moveConluded))	
                     	msg = self.Wcomms.packData(str(move), current, voltage, power, cumpower)
                     	# print(msg)
                     	sock.sendall(msg)
